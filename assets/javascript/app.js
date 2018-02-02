7714f474b2c0bb5be4a5be9f2243f3e945d08f5e
David Booth-Metwally <davidbmetwally@gmail.com>
2018-02-01 18:58:10 -0800

    added jumbotron

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,23 +18,30 @@ $(document).ready(function(){
 	/*function empties container before doing anything, then loops through film array and creates buttons for each string, it also adds attributes and classes to buttons before appending to page*/
 	function generateButtons(){
 		$("#sf-film-button").empty();
+	/* .keeps current buttons from duplicating upon pressing submit button*/
 		for (var i = 0; i < sfFilms.length; i++) {
+	/*iterates through each element in array to execute below*/
 			var filmBtn = $("<button>");
+	/* button assigned  */
 		    filmBtn.addClass("movie");
+	/* assigns class of 'movie' assigned */
 		    filmBtn.attr("data-name",sfFilms[i])
+	/* assigns attribute 'data-name' and iterates through array to provide according names to buttons  */
 		    filmBtn.text(sfFilms[i]);
+	/* text on buttons displayed according to interation */
 		    $("#sf-film-button").append(filmBtn);
 		};	
 	}
 
-	// call function immediatgely beause we want the buttons to show up when the page loads
+	// call function immediately because we want the buttons to show up when the page loads
 	generateButtons();
 
 	/* on click function clears image container before doing anything, grabs attribute value of data name of the button clicked, concatenates the value to the query url. using ajax , make call against giffy api which in the response, call another function and pass response array to that function */ 
 	$(document).on("click", ".movie", function(){
+	/*empty image container upon each click of button to clear previous appendages */
 		$("#image-container").empty();
 
-		console.log("i clicked a button");
+		// console.log("i clicked a button");
 
 
 		//clicked buttons attribute value
@@ -42,14 +49,15 @@ $(document).ready(function(){
 		console.log(movie)
 
 		// //base url + clicked button attribute value and api key and other paramater
-		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=Lk15PiiU3gyFxD2FrKFM48Q367ab4QwK&limit=10";
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&limit=10" + apiKey;
 		console.log(queryURL)
 		$.ajax({
 		  url: queryURL,
 		  method: "GET"
+		// ajax call and response from giffy api
 		}).then(function(response) {
 			console.log(response.data)
-
+		// assign variable to the response of api object and delve into the data key
 			var arrayOfAllGifs = response.data;
 
 			//function call that renders results, pass in response array
@@ -64,27 +72,41 @@ $(document).ready(function(){
 		for(var i=0; i < arr.length; i++ ){
 			//console.log(arr[i]);
 			var container = $("<div>");
+	//create div iterating through array to places for images
 			container.addClass("image-wrapper")
+	// add a class to the div
 			var image = $("<img>");
+	// create an image tag to hold the images
 			image.attr("src", arr[i].images.fixed_width_still.url);
+	// add src attribute to link images in the divs
 			image.attr("data-state", "still")
+	// add attributes for the date states for when animation/still
 			image.attr("data-animate", arr[i].images.fixed_width.url)
 			image.attr("data-still", arr[i].images.fixed_width_still.url )
+	// create a p tag for text		
 			var myRating = $("<p>");
+	// add text to html including the data from response that holds the rating
 			myRating.text("Rating: " + arr[i].rating);
+	// append response image holder and rating holder to the dom
 			$(container).append(image, myRating);
+	// prepend image container so the response results list down
 			$("#image-container").prepend(container)
 		}
 	}
 
 	/*on click of image, grab the value of the image clicked attribute, make an evaluation according to what that value is equal*/
 	$(document).on("click", "img", function(){
+	// on click of image sets condition for data-state attribute
 		var state = $(this).attr('data-state');
-
+	// image click attribute data state
 		if (state === "still") {
+	// if data-state is still
 	        $(this).attr("src", $(this).attr("data-animate"));
+	//attribute src is set to attribute data animate on click
 	        $(this).attr("data-state", "animate");
       	} else {
+    // if it is not still, the image src will change to the still image and change from
+    // animate to still
 	        $(this).attr("src", $(this).attr("data-still"));
 	        $(this).attr("data-state", "still");
       	}
